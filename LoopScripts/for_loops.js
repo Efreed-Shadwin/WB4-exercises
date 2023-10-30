771e7ea8712b6de2c531cb8803fd6c29b129df3d
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-10-30 13:20:49 -0500

    Added loops

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = "I love loops!";
+let i = 0;
+for (i+0; i < 7; i++){
+    console.log(message);
+}   
---
+++ LoopScripts/while-loops.js
@@ -0,0 +1,7 @@
+"use strict";
+let message = "I love loops!";
+let i = 0;
+while (i < 7){
+    console.log(message);
+    i++;
+}   
