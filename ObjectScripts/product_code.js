4b083712cb35885a440526a3d30becf0a7c6a433
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-10-30 13:09:35 -0500

    Fixed the function

--- ObjectScripts/product_code.js
+++ ObjectScripts/product_code.js
@@ -1,9 +1,10 @@
 "use strict";
 function parsePartCode(){
-    let partCode = {supplierCode: "someValue", productNumber: "someValue", size: "someValue"}
+    let partCode = {supplierCode: partCode1.slice(0,partCode1.indexOf(":")), productNumber: partCode1.slice(partCode1.indexOf(":")+1,partCode1.indexOf("-")), 
+                    size: partCode1.slice(partCode1.indexOf("-"))}
     return partCode;
 }
-let partCode1 = parsePartCode("XYZ:1234-L");
+let partCode1 = "XYZ:1234-L";
 let part1 = parsePartCode(partCode1);
 console.log("Supplier: "+ part1.supplierCode + " Product Number: " +
              part1.productNumber + " Size: " + part1.size);
